0cd21ca07a3be734df8d27327ae16cfc374c4579
Alexis Cañas <alexis91296@gmail.com>
2020-02-29 00:18:48 -0500

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+
+    $(window).scroll(function(){
+        let info = $(this).scrollTop()
+        console.log(info)
+        if (info>=40) {
+            // $('#navbar').removeClass('py-4','shadow-sm')
+            $('#navbar').removeClass('navbar-transparent').addClass('navbar-colored');
+            // $('#logo-navbar').css('height','30px');
+            // $('#logo-navbar').css('opacity','0');
+            // $('#corp-name').css('opacity','1');
+        }
+
+        if (info<40) {
+            // $('#navbar').addClass('py-4','shadow-sm')
+            $('#navbar').addClass('navbar-transparent').removeClass('navbar-colored')
+            // $('#logo-navbar').css('height','50px');
+            // $('#logo-navbar').css('opacity','1');
+            // $('#corp-name').css('opacity','0');
+        }
+    })
+})
+
+// Call carousel manually
+$('#myCarouselCustom').carousel();
+
+// Go to the previous item
+$("#prevBtn").click(function(){
+    $("#myCarouselCustom").carousel("prev");
+});
+// Go to the previous item
+$("#nextBtn").click(function(){
+    $("#myCarouselCustom").carousel("next");
+});
